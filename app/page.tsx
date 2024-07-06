d5f3f7118b675921c8b26bf9dfec917dac2b26d7
Martin Pilaszek <77743301+mapszk@users.noreply.github.com>
2024-07-06 19:15:03 -0300

    Finished page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,30 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+"use client";
+
 import PuzzlePiece from "@/components/client/PuzzlePiece";
-import PuzzlePieceServer from "@/components/server/PuzzlePiece";
 import Warning from "@/components/client/Warning";
-import WarningServer from "@/components/server/Warning";
+import { jsx, css } from "@emotion/react";
+
+const mainStyles = css`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  .row {
+    margin-bottom: 64px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
 
 export default function Home() {
   return (
-    <main className="flex flex-col">
-      <div className="flex gap-4 mb-12">
+    <main css={mainStyles}>
+      <div className="row">
         <PuzzlePiece
           pieceNumber={1}
           title="Understanding needs"
@@ -19,35 +37,14 @@ export default function Home() {
           description="Lorem Ipsum is simply dummy text of the printin and typesetting industry. Lorem Ipsum has been the industry's standard..."
           iconUrl="/icon-1.svg"
         />
-        {/* <PuzzlePiece
+        <PuzzlePiece
           pieceNumber={3}
           title="Developing customized solutions"
           description="Lorem Ipsum is simply dummy text of the printin and typesetting industry. Lorem Ipsum has been the industry's standard..."
           iconUrl="/icon-1.svg"
-        /> */}
-        <Warning text="Below are some of the key technologies we use in our projects:" />
-      </div>
-      <div className="flex gap-4">
-        <PuzzlePieceServer
-          pieceNumber={1}
-          title="Understanding needs"
-          description="We identify who has the problem or need and provide a detailed description of what the problem entails."
-          iconUrl="/icon-1.svg"
         />
-        <PuzzlePieceServer
-          pieceNumber={2}
-          title="Identifying opportunities"
-          description="Lorem Ipsum is simply dummy text of the printin and typesetting industry. Lorem Ipsum has been the industry's standard..."
-          iconUrl="/icon-1.svg"
-        />
-        {/* <PuzzlePieceServer
-          pieceNumber={3}
-          title="Developing customized solutions"
-          description="Lorem Ipsum is simply dummy text of the printin and typesetting industry. Lorem Ipsum has been the industry's standard..."
-          iconUrl="/icon-1.svg"
-        /> */}
-        <WarningServer text="Below are some of the key technologies we use in our projects:" />
       </div>
+      <Warning text="Below are some of the key technologies we use in our projects:" />
     </main>
   );
 }
