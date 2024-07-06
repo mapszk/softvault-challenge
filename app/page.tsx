4459521e70b9986f1db16005f5e093bdead9d373
Martin Pilaszek <77743301+mapszk@users.noreply.github.com>
2024-07-06 14:57:17 -0300

    Added tests

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,49 @@
+import PuzzlePiece from "@/components/client/PuzzlePiece";
+import PuzzlePieceServer from "@/components/server/PuzzlePiece";
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main className="flex flex-col">
+      <div className="flex gap-4 mb-12">
+        <PuzzlePiece
+          pieceNumber={1}
+          title="Understanding needs"
+          description="We identify who has the problem or need and provide a detailed description of what the problem entails."
+          iconUrl="/icon-1.svg"
+        />
+        <PuzzlePiece
+          pieceNumber={2}
+          title="Identifying opportunities"
+          description="Lorem Ipsum is simply dummy text of the printin and typesetting industry. Lorem Ipsum has been the industry's standard..."
+          iconUrl="/icon-1.svg"
+        />
+        <PuzzlePiece
+          pieceNumber={3}
+          title="Developing customized solutions"
+          description="Lorem Ipsum is simply dummy text of the printin and typesetting industry. Lorem Ipsum has been the industry's standard..."
+          iconUrl="/icon-1.svg"
+        />
+      </div>
+      <div className="flex gap-4">
+        <PuzzlePieceServer
+          pieceNumber={1}
+          title="Understanding needs"
+          description="We identify who has the problem or need and provide a detailed description of what the problem entails."
+          iconUrl="/icon-1.svg"
+        />
+        <PuzzlePieceServer
+          pieceNumber={2}
+          title="Identifying opportunities"
+          description="Lorem Ipsum is simply dummy text of the printin and typesetting industry. Lorem Ipsum has been the industry's standard..."
+          iconUrl="/icon-1.svg"
+        />
+        <PuzzlePieceServer
+          pieceNumber={3}
+          title="Developing customized solutions"
+          description="Lorem Ipsum is simply dummy text of the printin and typesetting industry. Lorem Ipsum has been the industry's standard..."
+          iconUrl="/icon-1.svg"
+        />
+      </div>
+    </main>
+  );
 }
---
+++ components/server/PuzzlePiece/PuzzlePiece.test.tsx
@@ -0,0 +1,51 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import PuzzlePiece from "./index";
+
+describe("Puzzle Piece component", () => {
+  const props = {
+    title: "Title",
+    description: "Description",
+    pieceNumber: 1,
+    iconUrl: "/icon-1.svg",
+  };
+
+  it("renders a heading", () => {
+    render(
+      <PuzzlePiece
+        title={props.title}
+        description={props.description}
+        iconUrl={props.iconUrl}
+        pieceNumber={props.pieceNumber}
+      />
+    );
+    const heading = screen.getByRole("heading", { level: 3 });
+    expect(heading).toBeInTheDocument();
+  });
+
+  it("renders a paragraph", () => {
+    render(
+      <PuzzlePiece
+        title={props.title}
+        description={props.description}
+        iconUrl={props.iconUrl}
+        pieceNumber={props.pieceNumber}
+      />
+    );
+    const paragraph = screen.getByRole("paragraph");
+    expect(paragraph).toBeInTheDocument();
+  });
+
+  it("renders an image", () => {
+    render(
+      <PuzzlePiece
+        title={props.title}
+        description={props.description}
+        iconUrl={props.iconUrl}
+        pieceNumber={props.pieceNumber}
+      />
+    );
+    const paragraph = screen.getByAltText("Icon");
+    expect(paragraph).toBeInTheDocument();
+  });
+});
